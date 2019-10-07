d1a6d90d96e5620cc160d1a69833f115dc155354
Petr Chavik <chavik.petr@gmail.com>
2019-10-07 20:38:04 +0200

    Update app icon

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/petr.chavik/Projects/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/petr.chavik/Projects/flutter_sudoku"
+export "FLUTTER_ROOT=/Users/petrc/projects/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/petrc/flutter_sudoku"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/petr.chavik/Projects/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/petrc/projects/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
